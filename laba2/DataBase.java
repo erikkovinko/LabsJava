18a278ebfa7a19433c6a988a1edeba75cd8ad44b
erikkovinko <101796960+erikkovinko@users.noreply.github.com>
2022-12-13 08:39:38 +0200

    Add files via upload

--- laba2/DataBase.java
+++ laba2/DataBase.java
@@ -2,9 +2,19 @@ package laba2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DataBase {
+	public void createTable() {
+		try {
+			Connection con = getConnection();
+			PreparedStatement create = con.prepareStatement("CREATE TABLE IF NO EXISTS pets(id in NOT NULL AUTO_INCREMENT, nameOfPet varchar(20), age INT(2), vaccinationStatus BOOLEAN, gender INT(1),PRIMARY KEY(id))");
+        create.executeUpdate();
+		}catch (Exception e){
+            e.printStackTrace();
+        }
+	}
 	public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
